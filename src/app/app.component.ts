ed0041d8cb3124ea66ca86a189a80f2ef7016bc9
Aleksandr Novolokov <novolokov@pre-sustainability.com>
2018-11-22 14:34:03 +0100

    reproduce: onSaveReport not called

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var Stimulsoft;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'stimulsoft';
+
+  designer = new Stimulsoft.Designer.StiDesigner(null, 'StiViewer', false);
+  report = new Stimulsoft.Report.StiReport();
+
+  constructor() {
+    this.designer.onSaveReport = (e) => {
+      console.log(e);
+      this.saveReportTemplate(e.report.saveToJsonString());
+    };
+  }
+
+  ngOnInit() {
+    this.designer.renderHtml('designerContent');
+  }
+
+  saveReportTemplate(Content) {
+    console.log('calling server, etc...', {Content, Id: 'someid', Name: 'reporting project name'});
+  }
 }
